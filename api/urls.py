2c5e71be60b2685da7c504164d77f9be59fea365
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2021-12-12 11:57:34 +0500

    task, homework views create

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from .views import CategoryListView, TeacherListCreateView
+from .views import (
+    CategoryListView, TaskListViewListCreateAPIView,TaskDetail,
+    HomeworkViewListCreateAPIView, TeacherListCreateView,
+    TeacherUpdateAPIView, TeacherDestroyAPIView,TeacherRetrieveAPIView
+)
+
 
 urlpatterns = [
     path("category/", CategoryListView.as_view()),
     path("teachers/", TeacherListCreateView.as_view()),
+    path("teachers/<int:pk>/", TeacherRetrieveAPIView.as_view()),
+    path("teachers/<int:pk>/update/", TeacherUpdateAPIView.as_view()),
+    path("teachers/<int:pk>/delete/", TeacherDestroyAPIView.as_view()),
+    path("task/", TaskListViewListCreateAPIView.as_view()),
+    path("task/<int:pk>/", TaskDetail.as_view()),
+    path("homework/", HomeworkViewListCreateAPIView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,14 +1,54 @@
 from rest_framework import generics
-from .serializers import CategorySerializer, TeacherSerializer
+from .serializers import CategorySerializer,TaskSerializer,HomeworkSerializer,TeacherSerializer
 from .models import Categories, Teacher, Course, Video, Task, Homework
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+
+#GET and POST
+class TeacherListCreateView(generics.ListCreateAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+# GET_ID
+class TeacherRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+
+# PUT_ID
+class TeacherUpdateAPIView(generics.UpdateAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+
+# DELETE_ID
+class TeacherDestroyAPIView(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
 
 # Create your views here.
 class CategoryListView(generics.ListAPIView):
     queryset = Categories.objects.all()
     serializer_class = CategorySerializer
 
+class TaskListViewListCreateAPIView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
 
-#GET and POST
-class TeacherListCreateView(generics.ListCreateAPIView):
-    queryset = Teacher.objects.all()
-    serializer_class = TeacherSerializer
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class HomeworkViewListCreateAPIView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Homework.objects.all()
+    serializer_class = HomeworkSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+        
+
+
